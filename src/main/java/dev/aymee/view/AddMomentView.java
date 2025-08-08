625304218c5f28d17fb821e7a682dd093049c03e
Aymee10 <areinar31@gmail.com>
2025-08-08 14:44:27 +0200

    fix DIA-4/Fixed import in AddMomentView

--- src/main/java/dev/aymee/view/AddMomentView.java
+++ src/main/java/dev/aymee/view/AddMomentView.java
@@ -2,7 +2,7 @@ package dev.aymee.view;
 import java.time.LocalDate;
 import java.util.Scanner;
 import dev.aymee.model.*;
-import java.time.LocalDate;
+//import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
