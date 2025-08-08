25758d818ff1609aa705ddab9e68be2e8bd5909d
Aymee10 <areinar31@gmail.com>
2025-08-08 13:06:57 +0200

    feat DIA-4/Created AddMomentView

--- src/view/AddMomentView.java
+++ src/view/AddMomentView.java
@@ -1,3 +1,42 @@
+import java.util.Scanner;
+
+
 public class AddMomentView {
-    
+  Scanner scanner;
+
+  public AddMomentView(Scanner scanner) {
+    this.scanner = scanner;
+  }
+  
+  public String askTitle(){
+    System.out.println("Ingrese el título del momento");
+    return scanner.nextLine();
+  }
+
+   public String askDescription(){
+    System.out.println("Ingrese la descripción del momento");
+    return scanner.nextLine();
+  }
+
+  public Emotion  askEmotion(){
+     System.out.println("Seleccione una emoción:");
+     Emotion[] emotions=Emotion.values;
+
+     for (int i = 0; i < emociones.length; i++) {
+            System.out.println((i + 1) + ". " + emociones[i].name().charAt(0) + emociones[i].name().substring(1).toLowerCase());
+        }
+        int option=-1;
+        while (option<0||option>emotions.length) {
+            System.out.println("Seleccione el número de la opción");
+            try {
+                  option = Integer.parseInt(scanner.nextLine());
+                  if(option<0||option>emotions.length){
+                    System.out.println("El número seleccionado no es una opción válida. Intente de nuevo");
+                  }
+            } catch (Exception e) {
+                System.out.println("Opción no válida, solo admite números. Intente de nuevo");
+            }
+        }
+        return emotions[option-1];
+  }
 }
