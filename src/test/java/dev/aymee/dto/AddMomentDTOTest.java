0b15c749279b5fce04d2954b8fa04d5da085a2d6
Aymee10 <areinar31@gmail.com>
2025-08-14 20:01:40 +0200

    test/Added test for AddMomentDTO

---
+++ src/test/java/dev/aymee/dto/AddMomentDTOTest.java
@@ -0,0 +1,24 @@
+package dev.aymee.dto;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
+
+import dev.aymee.model.Emotion;
+
+public class AddMomentDTOTest {
+    @Test
+    public void AddMomentDto(){
+        LocalDate date=LocalDate.of(2021,2 ,2 );
+        AddMomentDTO dto=new AddMomentDTO("Momento Prueba1", "Descripcion prueba 1", Emotion.ANSIEDAD, date);
+
+        assertThat("Momento Prueba1", equalTo(dto.getTitle()));
+        assertThat("Descripcion prueba 1", equalTo(dto.getDescription()));
+        assertThat(Emotion.ANSIEDAD,equalTo(dto.getEmotion()));
+        assertThat(date,equalTo(dto.getMomentDate()));
+
+    }
+}
