097ae1a59a9059b57b229e62516aa1643730f567
Aymee10 <areinar31@gmail.com>
2025-08-14 10:10:53 +0200

    feat DIA-20/Created DeleleMomentView

--- src/main/java/dev/aymee/view/DeleteMomentView.java
+++ src/main/java/dev/aymee/view/DeleteMomentView.java
@@ -1,4 +1,11 @@
 package dev.aymee.view;
+
+import dev.aymee.repository.MomentsRepository;
+
 public class DeleteMomentView {
-    
+     MomentsRepository repository;
+
+     public DeleteMomentView() {
+     }
+
 }
