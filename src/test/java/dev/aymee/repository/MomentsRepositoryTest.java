7d11adf50366a2c22f90a50d5df5a258731c6869
Aymee10 <areinar31@gmail.com>
2025-08-18 11:20:52 +0200

    test/PAssed test for deleteMoment in MomentRepository

--- src/test/java/dev/aymee/repository/MomentsRepositoryTest.java
+++ src/test/java/dev/aymee/repository/MomentsRepositoryTest.java
@@ -9,6 +9,8 @@ import dev.aymee.model.Emotion;
 import dev.aymee.model.Moment;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
 import java.util.List;
 
 public class MomentsRepositoryTest {
@@ -47,6 +49,29 @@ public class MomentsRepositoryTest {
         assertThat(moments.size(), equalTo(2));
         assertThat(moments.get(0), equalTo(moment1));
         assertThat(moments.get(1), equalTo(moment2));
+    }
+    @Test
+    void shouldDeleteExistingMoment(){
+        Moment moment1 = new Moment(1, "Prueba Titulo1", "Prueba Descripción1", Emotion.ALEGRIA, LocalDate.of(2020, 1, 1));
+        Moment moment2 = new Moment(2, "Prueba Titulo2", "Prueba Descripción2", Emotion.NOSTALGIA, LocalDate.of(2025, 1, 1));
+         repository.addMoment(moment1);
+         repository.addMoment(moment2);
+         
+         assertThat(repository.deleteMoment(1), is(true));
+         assertThat(repository.findAll().size(), is(1));
+    }
+    @Test
+    void shouldDeleteNonExistingMoment(){
+        Moment moment1 = new Moment(1, "Prueba Titulo1", "Prueba Descripción1", Emotion.ALEGRIA, LocalDate.of(2020, 1, 1));
+        Moment moment2 = new Moment(2, "Prueba Titulo2", "Prueba Descripción2", Emotion.NOSTALGIA, LocalDate.of(2025, 1, 1));
+         repository.addMoment(moment1);
+         repository.addMoment(moment2);
+         
+         assertThat(repository.deleteMoment(5), is(false));
+         assertThat(repository.findAll().size(), is(2));
+
+
+
     }
 
 }
