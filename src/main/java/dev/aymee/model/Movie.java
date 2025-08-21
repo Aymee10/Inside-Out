412af810709f106b3e416dcc658b8de4ef20e6be
Aymee10 <areinar31@gmail.com>
2025-08-21 12:28:41 +0200

    feat/Added model class Movie

---
+++ src/main/java/dev/aymee/model/Movie.java
@@ -0,0 +1,50 @@
+package dev.aymee.model;
+
+import java.util.List;
+public class Movie {
+    private String idImdb;
+    private String title;
+    private List<String> genres;
+    private String emotion;
+    private int releaseYear;
+    public Movie(String idImdb, String title, List<String> genres, String emotion, int releaseYear) {
+        this.idImdb = idImdb;
+        this.title = title;
+        this.genres = genres;
+        this.emotion = emotion;
+        this.releaseYear = releaseYear;
+    }
+    public String getIdImdb() {
+        return idImdb;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public List<String> getGenres() {
+        return genres;
+    }
+    public String getEmotion() {
+        return emotion;
+    }
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+    public void setIdImdb(String idImdb) {
+        this.idImdb = idImdb;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public void setGenres(List<String> genres) {
+        this.genres = genres;
+    }
+    public void setEmotion(String emotion) {
+        this.emotion = emotion;
+    }
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    
+}
+
